0b6a9f4830da800e52e0423ccdd19ee495b711dc
darkbtf <darkbtf@gmail.com>
2020-05-07 20:15:09 +0800

    add prediction result syncing feature

--- data_manager/data_manager.py
+++ data_manager/data_manager.py
@@ -6,15 +6,36 @@ import pickle
 
 from gql.transport.requests import RequestsHTTPTransport
 
-COFACTS_GRAPHQL_SERVER = 'https://cofacts-api.g0v.tw/graphql'
+COFACTS_GRAPHQL_SERVER = 'https://cofacts-api.hacktabl.org/graphql'
+
+CATEGORY_ID_MAPPING = [
+  'kj287XEBrIRcahlYvQoS', # 0 中國影響力
+  'kz3c7XEBrIRcahlYxAp6', # 1 性少數與愛滋病
+  'lD3h7XEBrIRcahlYeQqS', # 2 女權與性別刻板印象
+  'lT3h7XEBrIRcahlYugqq', # 3 保健秘訣、食品安全
+  'lj2m7nEBrIRcahlY6Ao_', # 4 基本人權問題
+  'lz2n7nEBrIRcahlYDgri', # 5 農林漁牧政策
+  'mD2n7nEBrIRcahlYLAr7', # 6 能源轉型
+  'mT2n7nEBrIRcahlYTArI', # 7 環境生態保護
+  'mj2n7nEBrIRcahlYdArf', # 8 優惠措施、新法規、政策宣導
+  'mz2n7nEBrIRcahlYnQpz', # 9 科技、資安、隱私
+  'nD2n7nEBrIRcahlYwQoW', # 10 免費訊息詐騙
+  'nT2n7nEBrIRcahlY6QqF', # 11有意義但不包含在以上標籤
+  'nj2n7nEBrIRcahlY-gpc', # 12 無意義
+  'nz2o7nEBrIRcahlYBgqQ', # 13 廣告
+  'oD2o7nEBrIRcahlYFgpm', # 14 只有網址其他資訊不足
+  'oT2o7nEBrIRcahlYKQoM', # 15 政治、政黨
+  'oj2o7nEBrIRcahlYRAox' # 16 轉發協尋、捐款捐贈
+]
 
 class DataManager:
   def __init__(self):
     transport=RequestsHTTPTransport(
-      url=COFACTS_GRAPHQL_SERVER,
+      url=COFACTS_GRAPHQL_SERVER+'?userId=bert',
       use_json=True,
       headers={
           "Content-type": "application/json",
+          "x-app-secret": "rumors-ai"
       },
       verify=False
     )
@@ -37,9 +58,9 @@ class DataManager:
 
     print(article_files)
 
-    for article_file in article_files:
-      article = pickle.load(open('./data/articles/{}'.format(article_file), 'rb'))
-      self.articles[article['id']] = article
+    # for article_file in article_files:
+    #   article = pickle.load(open('./data/articles/{}'.format(article_file), 'rb'))
+    #   self.articles[article['id']] = article
 
     self.models = {}
 
@@ -125,12 +146,51 @@ class DataManager:
     self.update_article_list()
     self.update_articles()
 
-  def sync_out(self):
+  def write_labels(self):
+    files = list(os.listdir('./result'))
 
-    query_string = '''
-      {{
-        UpdateArticleTags(
-          id: "{}"
-        )
-      }}
-    '''.format()
+    latest_result = sorted(files, reverse=True)[0]
+
+    results = pickle.load(open('./result/{}'.format(latest_result), 'rb'))
+    
+    for model_name in results.keys():
+      model_result = results[model_name]
+      # TODO: change model_name in gql request
+
+      for article_id in model_result.keys():
+        
+        article = pickle.load(open('./data/articles/{}.pkl'.format(article_id), 'rb'))
+        
+        print(article)
+
+        categories = list(map(lambda element: element['category']['id'], article['articleCategories']))
+
+
+
+        labels = model_result[article_id]
+
+        for label in labels:
+          if CATEGORY_ID_MAPPING[label[0]] not in categories:
+            query_string = '''
+              mutation {{
+                CreateArticleCategory(
+                  articleId: "{}"
+                  categoryId: "{}"
+                ) {{
+                  articleId
+                  categoryId
+                }}
+              }}
+            '''.format(article_id, CATEGORY_ID_MAPPING[label[0]], model_name, label[1])
+
+            print(query_string)
+
+            query = gql(query_string)
+
+            self.client.execute(query)
+    
+    return
+
+  def sync_out(self):
+    self.write_labels()
+
