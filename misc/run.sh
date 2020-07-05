31db081bd0f675a370f7f9bce9796dfd9afd2af7
darkbtf <darkbtf@gmail.com>
2020-07-05 18:52:00 +0800

    fix run.sh port

--- misc/run.sh
+++ misc/run.sh
@@ -5,4 +5,4 @@ mv rumors-ai rumors-ai-prod
 cd ~/rumors-ai-stg
 FLASK_APP=server.py ENV=staging python3 -m flask run --port=5001
 cd ~/rumors-ai-prod
-FLASK_APP=server.py ENV=production python3 -m flask run --port=5001
+FLASK_APP=server.py ENV=production python3 -m flask run --port=5000
