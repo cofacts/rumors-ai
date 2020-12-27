a0fae5874ebf54072c37ab49c2d1018ad2ddcdfa
darkbtf <darkbtf@gmail.com>
2020-12-28 02:23:25 +0800

    support category mapping

--- server.py
+++ server.py
@@ -24,6 +24,25 @@ redis_client = redis.Redis(host=settings.REDIS_HOST,
 data_manager = DataManager()
 model_manager = ModelManager()
 
+DEFAULT_CATEGORY_MAPPING = {
+  0: 'kj287XEBrIRcahlYvQoS', # 中國影響力
+  1: 'kz3c7XEBrIRcahlYxAp6', # 性少數與愛滋病
+  2: 'lD3h7XEBrIRcahlYeQqS', # 女權與性別刻板印象
+  3: 'lT3h7XEBrIRcahlYugqq', # 保健秘訣、食品安全
+  4: 'lj2m7nEBrIRcahlY6Ao_', # 基本人權問題
+  5: 'lz2n7nEBrIRcahlYDgri', # 農林漁牧政策
+  6: 'mD2n7nEBrIRcahlYLAr7', # 能源轉型
+  7: 'mT2n7nEBrIRcahlYTArI', # 環境生態保護
+  8: 'mj2n7nEBrIRcahlYdArf', # 優惠措施、新法規、政策宣導
+  9: 'mz2n7nEBrIRcahlYnQpz', # 科技、資安、隱私
+  10: 'nD2n7nEBrIRcahlYwQoW', # 免費訊息詐騙
+  11: 'nT2n7nEBrIRcahlY6QqF', # 有意義但不包含在以上標籤
+  12: 'nj2n7nEBrIRcahlY-gpc', # 無意義
+  13: 'nz2o7nEBrIRcahlYBgqQ', # 廣告
+  14: 'oD2o7nEBrIRcahlYFgpm', # 只有網址其他資訊不足
+  15: 'oT2o7nEBrIRcahlYKQoM', # 政治、政黨
+  16: 'oj2o7nEBrIRcahlYRAox' # 轉發協尋、捐款捐贈
+}
 
 def remove_object_id(object):
     object['id'] = str(object['_id'])
@@ -151,13 +170,28 @@ def post_model():
 
     model_info['apiKey'] = token_urlsafe(32)
     model_info['approved'] = False
+    if model_info['categoryMapping'] is None:
+        model_info['categoryMapping'] = DEFAULT_CATEGORY_MAPPING
 
     db_client.models.insert_one(model_info)
 
     return remove_object_id(model_info)
 
-# POST /v1/categorize/
 
+# PATCH /v1/models/{id}
+
+@app.route('/v1/models/<id>', methods=['PATCH'])
+def patch_model(id):
+    model_info = request.get_json()
+
+    update_result = db_client.models.update_one({'_id': ObjectId(id)}, {
+                                                '$set': model_info}, upsert=False)
+
+    return jsonify(update_result)
+
+
+
+# POST /v1/categorize/
 
 '''
 input:
@@ -241,7 +275,6 @@ def finish_task():
             pass
         update_result = db_client.tasks.update_one({'_id': ObjectId(task['id'])}, {
                                                    '$set': {'result': task['result']}}, upsert=False)
-        print(update_result)
         res.append({
             'id': task['id'],
             'result': {
